097f89b075279b49615d42d7df84da50cfc519f2
Michael Mountain <michael.mountain@ayo4u.com>
2022-08-15 12:58:02 +0200

    Add firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'ng-lists-app',
+    appId: '1:480539209060:web:70b4b8948d9cc020e51e71',
+    storageBucket: 'ng-lists-app.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyA58sCcWtaIzO9tLa8MXF5dtpaX4-tQLwg',
+    authDomain: 'ng-lists-app.firebaseapp.com',
+    messagingSenderId: '480539209060',
+  },
   production: false
 };
 
